ac64d0092f159b132315bc5f75925eb0f3c2a50a
Bhimsen Padalkar <bhim.padalkar@gmail.com>
2016-03-28 22:15:52 +0530

    Showing progress in notificaion bar instead of logging.

--- app/src/main/java/com/security/basics/app/services/MyServices.java
+++ app/src/main/java/com/security/basics/app/services/MyServices.java
@@ -1,7 +1,12 @@
 package com.security.basics.app.services;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.support.v7.app.NotificationCompat.Builder;
+
+import com.security.basics.app.R;
 
 
 public class MyServices extends IntentService {
@@ -12,11 +17,36 @@ public class MyServices extends IntentService {
 
   @Override
   protected void onHandleIntent(Intent intent) {
-    if(intent.getAction().equals("com.security.basics.app.services.DOWNLOAD_DATA")) {
-      System.out.println("DOWNLOAD THE DATA");
+    if (intent.getAction().equals("com.security.basics.app.services.DOWNLOAD_DATA")) {
+      showNotification("Downloading...", "Download in progress");
+    } else if (intent.getAction().equals("com.security.basics.app.services.UPLOAD_DATA")) {
+      showNotification("Uploading...", "Upload in progress");
     }
-    else if(intent.getAction().equals("com.security.basics.app.services.UPLOAD_DATA")) {
-      System.out.println("UPLOAD THE DATA");
-    }
+  }
+
+  private void showNotification(String title, String message) {
+    final NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    final Builder builder = new Builder(this);
+    builder.setContentTitle(title)
+        .setContentText(message)
+        .setSmallIcon(R.drawable.ic_launcher);
+    new Thread(
+        new Runnable() {
+          @Override
+          public void run() {
+            for (int i = 0; i <= 100; i += 5) {
+              builder.setProgress(100, i, false);
+              notificationManager.notify(1, builder.build());
+              try {
+                Thread.sleep(5 * 1000);
+              } catch (InterruptedException ignored) {
+              }
+            }
+            builder.setContentText("Complete !!!")
+                .setProgress(0, 0, false);
+            notificationManager.notify(1, builder.build());
+          }
+        }
+    ).start();
   }
 }
